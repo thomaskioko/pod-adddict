50b0b70895402609710ab02d3df89d3a863462c0
Thomas Kioko <kiokotomas@gmail.com>
2016-09-28 21:22:10 +0300

    Create glide helper class.
    Classes used to load images using glide and modify the image

---
+++ app/src/main/java/com/thomaskioko/podadddict/app/util/glide/ApGlideSettings.java
@@ -0,0 +1,13 @@
+package com.thomaskioko.podadddict.app.util.glide;
+
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+/**
+ * The settings that PodAdddict will use for various Glide options
+ *
+ * @author Thomas kioko
+ */
+public class ApGlideSettings {
+
+    public static final DiskCacheStrategy AP_DISK_CACHE_STRATEGY = DiskCacheStrategy.ALL;
+}
