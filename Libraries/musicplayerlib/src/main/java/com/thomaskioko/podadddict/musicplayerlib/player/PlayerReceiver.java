3beb9f714dbf8d6692fdd2883bb6584c36635f17
kioko <kiokotomas@gmail.com>
2016-10-12 15:51:32 +0300

    Create reciever
    Player reciever used to listen to player noise actions

---
+++ Libraries/musicplayerlib/src/main/java/com/thomaskioko/podadddict/musicplayerlib/player/PlayerReceiver.java
@@ -0,0 +1,26 @@
+package com.thomaskioko.podadddict.musicplayerlib.player;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Receiver used to catch {@link android.media.AudioManager#ACTION_AUDIO_BECOMING_NOISY} events.
+ * <p/>
+ * Propagate event to the player.
+ *
+ * @author kioko
+ */
+public class PlayerReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(
+                android.media.AudioManager.ACTION_AUDIO_BECOMING_NOISY)) {
+            // signal player service to stop playback
+            Intent i = new Intent(context, PlaybackService.class);
+            i.setAction(PlaybackService.ACTION_AUDIO_BECOMING_NOISY);
+            context.startService(i);
+        }
+    }
+}
