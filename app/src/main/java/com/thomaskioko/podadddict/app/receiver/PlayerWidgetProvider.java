b14483a4eac4afcba1de8b4047aabce6bdbf6191
kioko <kiokotomas@gmail.com>
2016-10-19 15:28:03 +0300

    Add player control functionality.😊
    Allow the user to control the player using the widget.

--- app/src/main/java/com/thomaskioko/podadddict/app/receiver/PlayerWidgetProvider.java
+++ app/src/main/java/com/thomaskioko/podadddict/app/receiver/PlayerWidgetProvider.java
@@ -10,8 +10,11 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 import android.widget.RemoteViews;
 
+import com.thomaskioko.podadddict.app.PodAddictApplication;
 import com.thomaskioko.podadddict.app.R;
+import com.thomaskioko.podadddict.app.interfaces.Listener;
 import com.thomaskioko.podadddict.app.service.PlayerWidgetService;
+import com.thomaskioko.podadddict.musicplayerlib.player.PodAdddictPlayer;
 
 /**
  * Helper class to aid in implementing an AppWidget provider.
@@ -19,14 +22,18 @@ import com.thomaskioko.podadddict.app.service.PlayerWidgetService;
  * @author kioko.
  */
 
-public class PlayerWidgetProvider extends AppWidgetProvider {
+public class PlayerWidgetProvider extends AppWidgetProvider implements Listener {
     public static String ACTION_WIDGET_NEXT = "ActionReceiverNext";
     public static String ACTION_WIDGET_PREVIOUS = "ActionReceiverPrevious";
     public static String ACTION_WIDGET_PLAY = "ActionReceiverPlay";
 
+    private static PodAdddictPlayer mPodAdddictPlayer;
+
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        mPodAdddictPlayer = PodAddictApplication.getPodAdddictPlayer();
 
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.player_widget);
 
@@ -65,13 +72,40 @@ public class PlayerWidgetProvider extends AppWidgetProvider {
 
         if (intent.getAction().equals(ACTION_WIDGET_NEXT)) {
             Log.i("onReceive", ACTION_WIDGET_NEXT);
+            if(mPodAdddictPlayer.isPlaying())
+            this.onNextPressed();
         } else if (intent.getAction().equals(ACTION_WIDGET_PREVIOUS)) {
             Log.i("onReceive", ACTION_WIDGET_PREVIOUS);
+            if(mPodAdddictPlayer.isPlaying())
+            this.onPreviousPressed();
         } else if (intent.getAction().equals(ACTION_WIDGET_PLAY)) {
             Log.i("onReceive", ACTION_WIDGET_PLAY);
+            if(mPodAdddictPlayer.isPlaying())
+            this.onTogglePlayPressed();
         } else {
             super.onReceive(context, intent);
         }
     }
 
+
+
+    @Override
+    public void onTogglePlayPressed() {
+        mPodAdddictPlayer.togglePlayback();
+    }
+
+    @Override
+    public void onPreviousPressed() {
+        mPodAdddictPlayer.previous();
+    }
+
+    @Override
+    public void onNextPressed() {
+        mPodAdddictPlayer.next();
+    }
+
+    @Override
+    public void onSeekToRequested(int milli) {
+        mPodAdddictPlayer.seekTo(milli);
+    }
 }
