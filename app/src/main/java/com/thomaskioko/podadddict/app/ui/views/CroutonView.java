fd1b8cdb984560cf4f9b310cc3b0be18c7c420cc
Thomas Kioko <kiokotomas@gmail.com>
2016-10-24 22:10:09 +0300

    Create custom view
    This will display a notificaiton when an podcast is added to the playlist.

---
+++ app/src/main/java/com/thomaskioko/podadddict/app/ui/views/CroutonView.java
@@ -0,0 +1,44 @@
+package com.thomaskioko.podadddict.app.ui.views;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.thomaskioko.podadddict.app.R;
+
+
+/**
+ * Simple compoundView used to display custom crouton.
+ * <p/>
+ * Basically, simply add a shadow.
+ */
+public class CroutonView extends LinearLayout {
+
+    /**
+     * CompoundView to display a person.
+     *
+     * @param context calling context.
+     * @param text    displayed.
+     */
+    public CroutonView(Context context, String text) {
+        super(context);
+        if (!isInEditMode()) {
+            init(context, text);
+        }
+    }
+
+    /**
+     * Initialize internal component.
+     *
+     * @param context calling context.
+     * @param text    text displayed.
+     */
+    private void init(Context context, String text) {
+        this.setOrientation(VERTICAL);
+        LayoutInflater.from(context).inflate(R.layout.crouton_view, this);
+        ((TextView) this.findViewById(R.id.crouton_view_message)).setText(text);
+
+        setOrientation(VERTICAL);
+    }
+}
